fe3251f69d9d8c28a98353c4a2ccb5ef391eb24c
Dennis Wuitz <dennish@wuitz.de>
2023-12-25 04:27:28 +0100

    add fulldiskencryption

--- systems/photon/configuration.nix
+++ systems/photon/configuration.nix
@@ -9,6 +9,7 @@
   i18n.supportedLocales = [ "de_DE.UTF-8/UTF-8" ];
 
   networking.hostId = "7d76fab7";
+  boot.initrd.network.ssh.authorizedKeys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJYHv/LMo8N6iM3zFvOKrF7ZLp3eAG/cOED0yDzrvgkd openpgp:0x74CCE9B8" ];
 
   services = {
     nginx = {

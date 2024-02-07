5a0c67f2d14e22a5d0abda832827af279e1291e1
ahuston-0 <aliceghuston@gmail.com>
2024-02-06 22:17:36 -0500

    Add artemision
    Signed-off-by: ahuston-0 <aliceghuston@gmail.com>

---
+++ users/alice/systems/artemision/configuration.nix
@@ -0,0 +1,44 @@
+{ pkgs, ... }: {
+  imports = [ ../configuration.nix ../programs.nix ./programs.nix ];
+
+  time.timeZone = "America/New_York";
+  console.keyMap = "us";
+
+  networking = {
+    hostId = "58f50a15";
+    firewall.enable = true;
+  };
+
+  boot = {
+    useSystemdBoot = true;
+    default = true;
+  };
+
+
+  i18n = {
+    defaultLocale = "en_US.utf8";
+    supportedLocales = [ "en_US.UTF-8/UTF-8" ];
+  };
+
+  virtualisation = {
+    docker = {
+      enable = true;
+      recommendedDefaults = true;
+      logDriver = "local";
+      storageDriver = "overlay2";
+      daemon."settings" = {
+        experimental = true;
+        data-root = "/var/lib/docker";
+        exec-opts = [ "native.cgroupdriver=systemd" ];
+        log-opts = {
+          max-size = "10m";
+          max-file = "5";
+        };
+      };
+    };
+
+  };
+
+
+  system.stateVersion = "24.05";
+}
---
+++ users/alice/systems/programs.nix
@@ -0,0 +1,48 @@
+{ pkgs, ... }:
+{
+  environment.systemPackages = with pkgs; [
+    bfg-repo-cleaner
+    candy-icons
+    calibre
+    # calibre dedrm?
+    discord-canary
+    etcher
+    fanficfare
+    ferium
+    # gestures replacement
+    gpu-viewer
+    headsetcontrol
+    ipmiview
+    ipscan
+    masterpdfeditor4
+    mons
+    # nbt explorer?
+    noisetorch
+    ocrmypdf
+    pinentry-rofi
+    playonlinux
+    protonmail-bridge
+    protontricks
+    redshift
+    ripgrep
+    rpi-imager
+    rofi-wayland
+    # signal in tray?
+    siji
+    simple-mtpfs
+    slack-desktop
+    snyk
+    spotify
+    spotify-player
+    #swaylock/waylock?
+    sweet-nova
+    unipicket
+    ventoy
+    visual-studio-code
+    watchman
+    wired
+    xboxdrv
+    yubico-authenticator-bin
+    zoom
+  ];
+}

74635b55cc791e34d21c32a998e014fef0baf407
ahuston-0 <aliceghuston@gmail.com>
2024-02-17 23:31:58 -0500

    add attic to substitutors
    Signed-off-by: ahuston-0 <aliceghuston@gmail.com>

--- flake.nix
+++ flake.nix
@@ -4,12 +4,14 @@
   nixConfig = {
     trusted-users = [ "root" ];
     substituters = [
+      "https://attic.alicehuston.xyz/cache-nix-dot"
       "https://cache.nixos.org"
       "https://cache.alicehuston.xyz"
       "https://nix-community.cachix.org"
     ];
 
     trusted-substituters = [
+      "https://attic.alicehuston.xyz/cache-nix-dot"
       "https://cache.nixos.org"
       "https://cache.alicehuston.xyz"
       "https://nix-community.cachix.org"
@@ -19,6 +21,7 @@
       "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
       "cache.alicehuston.xyz:SJAm8HJVTWUjwcTTLAoi/5E1gUOJ0GWum2suPPv7CUo=%"
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "cache-nix-dot:lxA/41loRSMmzLScnKR0B4SQV5eWYIleS9VR6PbYJTE="
     ];
   };
 

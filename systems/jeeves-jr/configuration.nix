be5132cad79f8b97c045d0906bacf54768d068f7
Richie Cahill <richie@tmmworkshop.com>
2024-02-19 10:18:15 -0500

    test

--- systems/jeeves-jr/configuration.nix
+++ systems/jeeves-jr/configuration.nix
@@ -52,10 +52,21 @@
       exporters = {
         node = {
           enable = true;
-          enabledCollectors = [ "systemd" ];
+          enabledCollectors = ["systemd"];
           port = 9002;
         };
       };
+
+      scrapeConfigs = [
+        {
+          job_name = "chrysalis";
+          static_configs = [
+            {
+              targets = ["127.0.0.1:${toString config.services.prometheus.exporters.node.port}"];
+            }
+          ];
+        }
+      ];
     };
 
     usbguard = {

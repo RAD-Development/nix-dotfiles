feed2eba63f5ab789254a8ea1d289bc453dd424b
Richie Cahill <richie@tmmworkshop.com>
2024-02-19 10:19:58 -0500

    test

--- systems/jeeves-jr/configuration.nix
+++ systems/jeeves-jr/configuration.nix
@@ -62,7 +62,7 @@
           job_name = "chrysalis";
           static_configs = [
             {
-              targets = ["127.0.0.1:${toString config.services.prometheus.exporters.node.port}"];
+              targets = ["127.0.0.1:9002"];
             }
           ];
         }

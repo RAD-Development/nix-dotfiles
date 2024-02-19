c55652f494cd6ddb7330c227ceb3d1d876f822fe
Richie Cahill <richie@tmmworkshop.com>
2024-02-19 11:00:18 -0500

    test

--- systems/jeeves-jr/configuration.nix
+++ systems/jeeves-jr/configuration.nix
@@ -81,12 +81,20 @@
               targets = ["127.0.0.1:9134"];
             }
           ];
+        }
+      ];
+      scrapeConfigs = [
+        {
           job_name = "systemd";
           static_configs = [
             {
               targets = ["127.0.0.1:9558"];
             }
           ];
+        }
+      ];
+      scrapeConfigs = [
+        {
           job_name = "systemd";
           static_configs = [
             {

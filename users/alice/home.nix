e051c2417bf921b521c0ba1acb18d03dc9ff82a2
ahuston-0 <aliceghuston@gmail.com>
2024-03-02 16:44:21 -0500

    restore home-manager
    Signed-off-by: ahuston-0 <aliceghuston@gmail.com>

--- users/alice/home.nix
+++ users/alice/home.nix
@@ -25,29 +25,29 @@
       ncdu
 
       # Rust packages
-      # trunk
-      # wasm-pack
-      # cargo-watch
-      # #pkgs.cargo-tarpaulin
-      # cargo-generate
-      # cargo-audit
-      # cargo-update
-      # diesel-cli
-      # gitoxide
-      # tealdeer
-      # helix
+      trunk
+      wasm-pack
+      cargo-watch
+      #pkgs.cargo-tarpaulin
+      cargo-generate
+      cargo-audit
+      cargo-update
+      diesel-cli
+      gitoxide
+      tealdeer
+      helix
 
-      # nix specific packages
-      # nil
-      # nixfmt
+      nix specific packages
+      nil
+      nixfmt
 
-      # # markdown
-      # nodePackages.markdownlint-cli
+      # markdown
+      nodePackages.markdownlint-cli
 
-      # # doom emacs dependencies
-      # fd
-      # ripgrep
-      # clang
+      # doom emacs dependencies
+      fd
+      ripgrep
+      clang
     ];
   };
 

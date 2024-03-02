7b48a96ec2729b67e546601b3b9dcd54a58002c1
ahuston-0 <aliceghuston@gmail.com>
2024-03-02 16:10:25 -0500

    remove richie
    Signed-off-by: ahuston-0 <aliceghuston@gmail.com>

--- users/richie/home.nix
+++ users/richie/home.nix
@@ -7,31 +7,31 @@
     homeDirectory = "/home/richie";
     packages = with pkgs; [
       # Rust packages
-      topgrade
-      trunk
-      wasm-pack
-      cargo-watch
-      # pkgs.cargo-tarpaulin
-      cargo-generate
-      cargo-audit
-      cargo-update
-      diesel-cli
-      # gitoxide currently broke 09182023
-      gitoxide
-      tealdeer
-      helix
+      # topgrade
+      # trunk
+      # wasm-pack
+      # cargo-watch
+      # # pkgs.cargo-tarpaulin
+      # cargo-generate
+      # cargo-audit
+      # cargo-update
+      # diesel-cli
+      # # gitoxide currently broke 09182023
+      # gitoxide
+      # tealdeer
+      # helix
 
       # nix specific packages
-      nil
-      nixfmt
+      # nil
+      # nixfmt
 
-      # markdown
-      nodePackages.markdownlint-cli
+      # # markdown
+      # nodePackages.markdownlint-cli
 
-      # doom emacs dependencies
-      fd
-      ripgrep
-      clang
+      # # doom emacs dependencies
+      # fd
+      # ripgrep
+      # clang
     ];
   };
 

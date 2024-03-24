c12929befc2fcc9c6e96c4319520a48a3309d1ed
ahuston-0 <alice.huston@gmail.com>
2024-03-24 00:19:57 -0400

    update subs

--- flake.nix
+++ flake.nix
@@ -3,21 +3,24 @@
 
   nixConfig = {
     substituters = [
-      "https://cache.nixos.org"
-      "https://nix-community.cachix.org"
-      "https://cache.alicehuston.xyz"
+      "https://cache.nixos.org/?priority=1&want-mass-query=true"
+      "https://attic.alicehuston.xyz/cache-nix-dot?priority=4&want-mass-query=true"
+      "https://cache.alicehuston.xyz/?priority=5&want-mass-query=true"
+      "https://nix-community.cachix.org/?priority=10&want-mass-query=true"
     ];
     trusted-substituters = [
       "https://cache.nixos.org"
-      "https://nix-community.cachix.org"
+      "https://attic.alicehuston.xyz/cache-nix-dot"
       "https://cache.alicehuston.xyz"
+      "https://nix-community.cachix.org"
     ];
     trusted-public-keys = [
-      "cache.alicehuston.xyz:SJAm8HJVTWUjwcTTLAoi/5E1gUOJ0GWum2suPPv7CUo=%"
       "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+      "cache.alicehuston.xyz:SJAm8HJVTWUjwcTTLAoi/5E1gUOJ0GWum2suPPv7CUo=%"
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "cache-nix-dot:lxA/41loRSMmzLScnKR0B4SQV5eWYIleS9VR6PbYJTE="
     ];
-    trusted-users = [ "root" ];
+    trusted-users = [ "root" "@wheel" ];
   };
 
   inputs = {

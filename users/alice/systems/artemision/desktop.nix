af0bb537c99ef25f060a6f620e21fc7ce709fe18
ahuston-0 <aliceghuston@gmail.com>
2024-03-24 13:09:58 -0400

    fix formatting
    Signed-off-by: ahuston-0 <aliceghuston@gmail.com>

--- users/alice/systems/artemision/desktop.nix
+++ users/alice/systems/artemision/desktop.nix
@@ -13,11 +13,8 @@
   # Optional, hint electron apps to use wayland:
   environment.sessionVariables.NIXOS_OZONE_WL = "1";
 
-
   services.xserver.displayManager.gdm = {
     enable = true;
     # wayland.enable = true;
   };
-
-
 }

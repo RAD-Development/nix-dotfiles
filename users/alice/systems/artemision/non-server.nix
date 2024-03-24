17526d6d1dba7ead41709ca260e77cf5769f7004
ahuston-0 <aliceghuston@gmail.com>
2024-03-24 04:08:13 -0400

    non-server :)
    Signed-off-by: ahuston-0 <aliceghuston@gmail.com>

--- users/alice/systems/artemision/non-server.nix
+++ users/alice/systems/artemision/non-server.nix
@@ -72,10 +72,21 @@
       connect-timeout = 20;
     };
 
+    # free up to 10 gb when only 1 gb left
+    extraOptions = ''
+      min-free = ${toString (1 * 1024 * 1024 * 1024)}
+      max-free = ${toString (10 * 1024 * 1024 * 1024)}
+    '';
+
     gc = {
       automatic = true;
       dates = "weekly";
       options = "--delete-older-than 30d";
+    };
+
+    optimise = {
+      automatic = true;
+      dates = [ "01:00" ];
     };
   };
 

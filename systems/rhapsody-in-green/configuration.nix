2dba711be05b3cae3296c9ae96edc93094d229f3
Richie Cahill <Richie@tmmworkshop.com>
2024-04-28 14:07:41 -0400

    testing

--- systems/rhapsody-in-green/configuration.nix
+++ systems/rhapsody-in-green/configuration.nix
@@ -14,6 +14,7 @@
   networking = {
     networkmanager.enable = true;
     hostId = "9b68eb32";
+    firewall.enable = false;
   };
 
   hardware = {
@@ -98,7 +99,21 @@
   services = {
     mongodb = {
       enable = true;
+      dbpath = "/home/richie/mongodb";
+      user = "richie";
     };
+    # opensearch = {
+    #   enable = true;
+    #   user = "richie";
+    #   group = "users";
+    #   dataDir = "/home/richie/opensearch";
+    #   settings ={
+    #     "plugins.security.disabled"=true;
+    #     "plugins.security.ssl.http.enabled"=false;
+    #     "action.auto_create_index"=false;
+    #     "bootstrap.memory_lock"=true;
+    #   };
+    # }; 
     elasticsearch = {
       enable = true;
     };
@@ -107,8 +122,15 @@
       passwordSecret = "LfjRKrrbONYDCvfD8gCNYqMzVAPHrxVBaw1oR3zIE73cF0EUaj8yEU4DsY8ADbwlGKCt0f2Q9Di8CN6JCYqGug2cfUwE9oNG";
       rootPasswordSha2 = "e3c652f0ba0b4801205814f8b6bc49672c4c74e25b497770bb89b22cdeb4e951";
       elasticsearchHosts = [ "http://localhost:9200" ];
+      isMaster = true;
+    };
+    rsyslogd = {
+      enable = true;
+      extraConfig = "*.* @localhost:6514};RSYSLOG_SyslogProtocol23Format";
+      # extraConfig = "*.* @${cfg.rsyslogServer};RSYSLOG_SyslogProtocol23Format";
     };
   };
+
   system.autoUpgrade.enable = false;
   system.stateVersion = "23.11";
 }

eba9d9c4cd9767628eda2ced8daca4b0015fcf5d
ahuston-0 <aliceghuston@gmail.com>
2024-05-25 16:03:38 -0400

    idek what i did to fix this
    Signed-off-by: ahuston-0 <aliceghuston@gmail.com>

--- lib/microvms.nix
+++ lib/microvms.nix
@@ -3,14 +3,16 @@ rec {
   genK3SVM =
     server-config: agent-config: vms:
     lib.mapAttrs (
-      host: {ipv4,server ? false}:
-      genMicroVM host ipv4 "x86_64-linux" (
-        if server then (import server-config) else (import agent-config)
-      )
+      host:
+      {
+        ipv4,
+        server ? false,
+      }:
+      genMicroVM host ipv4 "x86_64-linux" (if server then server-config else agent-config)
     ) vms;
 
   genMicroVM =
-    hostName: ipv4: system: vm-config:
+    hostName: ipv4: _system: vm-config:
     # microvm refers to microvm.nixosModules
 
     # {
@@ -20,37 +22,40 @@ rec {
     #   ...
     # }:
     {
-        # The package set to use for the microvm. This also determines the microvm's architecture.
-        # Defaults to the host system's package set if not given.
-        # pkgs = import pkgs { inherit system; };
+      # The package set to use for the microvm. This also determines the microvm's architecture.
+      # Defaults to the host system's package set if not given.
+      # pkgs = import pkgs { inherit system; };
 
-        # (Optional) A set of special arguments to be passed to the MicroVM's NixOS modules.
-        #specialArgs = {};
+      # (Optional) A set of special arguments to be passed to the MicroVM's NixOS modules.
+      #specialArgs = {};
 
-        # The configuration for the MicroVM.
-        # Multiple definitions will be merged as expected.
-        config = {
-          # It is highly recommended to share the host's nix-store
-          # with the VMs to prevent building huge images.
-          microvm.shares = [
+      # The configuration for the MicroVM.
+      # Multiple definitions will be merged as expected.
+      config = {
+        imports = [ vm-config ];
+        # It is highly recommended to share the host's nix-store
+        # with the VMs to prevent building huge images.
+        microvm.shares = [
+          {
+            source = "/nix/store";
+            mountPoint = "/nix/.ro-store";
+            tag = "ro-store";
+            proto = "virtiofs";
+          }
+        ];
+
+        networking = {
+          inherit hostName;
+          interfaces.ether.ipv4.addresses = [
             {
-              source = "/nix/store";
-              mountPoint = "/nix/.ro-store";
-              tag = "ro-store";
-              proto = "virtiofs";
-            }
-          ];
-
-          networking = {
-            inherit hostName;
-            interfaces.ether.ipv4.addreses = {
               address = ipv4;
               prefixLength = 32;
-            };
-          };
+            }
+          ];
+        };
 
-          # Any other configuration for your MicroVM
-          # [...]
-        } // vm-config;
+        # Any other configuration for your MicroVM
+        # [...]
+      };
     };
 }

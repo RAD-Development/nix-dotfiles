1c596593caa625c4448f9e2af72c74d727154867
ahuston-0 <aliceghuston@gmail.com>
2024-05-25 15:56:49 -0400

    try to fix attrset generation
    Signed-off-by: ahuston-0 <aliceghuston@gmail.com>

--- systems/palatine-hill/microvms.nix
+++ systems/palatine-hill/microvms.nix
@@ -6,22 +6,20 @@
   ...
 }:
 let
-  inherit (lib.rad-dev.microvm) genK3SFromList;
+  inherit (lib.rad-dev.microvm) genK3SVM;
 in
 
 {
   # rad-dev.microvm-host.enable = true;
   rad-dev.microvm-host.vms =
-    genK3SFromList (src + "/modules/opt/k3s-server.nix") (src + "/modules/opt/k3s-agent.nix")
-      [
-        {
-          host = "ph-server-1";
+    genK3SVM (src + "/modules/opt/k3s-server.nix") (src + "/modules/opt/k3s-agent.nix")
+      {
+        "ph-server-1" = {
           ipv4 = "192.168.69.10";
           server = true;
-        }
-        {
-          host = "ph-agent-1";
-          ipv4 = "192.168.69.30";
-        }
-      ];
+        };
+        # "ph-agent-1" = {
+        #   ipv4 = "192.168.69.30";
+        # };
+      };
 }

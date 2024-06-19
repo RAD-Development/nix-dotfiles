da63b9a0ce6fb973dca55fd2480fece1df01b35f
ahuston-0 <aliceghuston@gmail.com>
2024-06-19 16:15:04 -0400

    add comment for overseerr port
    Signed-off-by: ahuston-0 <aliceghuston@gmail.com>

--- systems/jeeves/docker/internal.nix
+++ systems/jeeves/docker/internal.nix
@@ -98,7 +98,7 @@
       TZ = "America/New_York";
     };
     volumes = [ "/ZFS/Media/Docker/Docker/Storage/overseerr:/config" ];
-    ports = [ "5055:5055" ];
+    ports = [ "5055:5055" ]; # Web UI port
     autoStart = true;
   };
 }

8ae35092c60664254992c419cece2e25d1787285
ahuston-0 <aliceghuston@gmail.com>
2024-06-29 12:11:59 -0400

    refactor utils, nextcloud image, add fetch-docker script
    Signed-off-by: ahuston-0 <aliceghuston@gmail.com>

--- systems/palatine-hill/docker/nextcloud-image/default.nix
+++ systems/palatine-hill/docker/nextcloud-image/default.nix
@@ -1,12 +1,6 @@
 { pkgs, ... }:
 let
-  nextcloud-apache = pkgs.dockerTools.pullImage {
-    imageName = "nextcloud";
-    imageDigest = "sha256:2ab0c5de2afd33c55721e69e87bd1667cad6c4550ba0fd0303cfeb7abb260750";
-    sha256 = "11fh4jaz1asiihf2092sh7ma9xm35hmnkafpbjr3igkv8jicsky1";
-    finalImageName = "nextcloud";
-    finalImageTag = "apache";
-  };
+  nextcloud-apache = pkgs.dockerTools.pullImage (import ./nextcloud-apache.nix);
 in
 
 pkgs.dockerTools.buildImage {
